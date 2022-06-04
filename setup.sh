39ffc6408745e7de65eed3029f2ebbbb551ae18b
SantiR <santirodriguez38@gmail.com>
2022-06-04 17:26:10 -0300

    Added setup configuration.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
+# Environment
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
+# Database
+mkdir src/database/
 python ./src/jw_news/models/migrations.py
+
+# Running bot tasks
+python ./src/brunito_bot.py
