df73359890157d6ba004decad694b01831595d4a
Santiago Rodriguez <santiago@comparaencasa.com>
2022-06-04 15:48:24 -0300

    Added date field.

--- src/jw_news/models.py
+++ src/jw_news/models.py
@@ -5,6 +5,7 @@ from db_connection import database
 
 class JWNews(peewee.Model):
     link = peewee.CharField(max_length=350, unique=True, index=True)
+    date_release = peewee.CharField(max_length=10)
     date_created = peewee.DateField(default=dt.now)
 
     class Meta:
