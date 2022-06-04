8bf79806fa86341b8adf251df829f70b560d18c5
Santiago Rodriguez <santiago@comparaencasa.com>
2022-06-04 14:16:50 -0300

    Created sqlite database.

---
+++ src/jw_news/models.py
@@ -0,0 +1,12 @@
+import peewee
+from datetime import datetime as dt
+
+from db_connection import database
+
+class JWNews(peewee.Model):
+    link = peewee.CharField(max_length=350, unique=True, index=True)
+    date_created = peewee.DateField(default=dt.now)
+
+    class Meta:
+        database = database
+        da_table = "JWNews"
