fd6ecc29eddde23a262fee74177c584adeb53af1
Santi Rodríguez <47932560+SantiR38@users.noreply.github.com>
2022-06-21 19:07:58 -0300

    Video updates (#6)
    * Consuming api for videos.
    
    * Deleted debugger.
    
    * Ignored jupiter files.
    
    * Added new group for jw news.
    
    * Video thumbnail formatted for telegram message.
    
    Co-authored-by: Santiago Rodriguez <santiago@comparaencasa.com>

--- src/jw_news/parser.py
+++ src/jw_news/parser.py
@@ -1,3 +1,4 @@
+from asyncio.log import logger
 import json
 import logging
 import requests
@@ -16,7 +17,57 @@ def dprint(data: dict, sort_keys: bool = False) -> None:
         print(json.dumps(data, indent=4, sort_keys=sort_keys))
 
 
-class Parser:
+class JWAPI:
+    def __init__(self):
+        """
+        Dates are in the format of YYYY-MM-DD
+        """
+        self.today = date.today().strftime("%Y-%m-%d")
+        self.yesterday = (date.today()-td(days=1)).strftime("%Y-%m-%d")
+        self.links = []
+
+    def _filter_content(self, content) -> bool:
+        article_is_old = content['date'] not in [self.today, self.yesterday]
+        is_repeated = content['link'] in self.links
+
+        if is_repeated or article_is_old:
+            return False
+
+        not_already_sended = JWNews.select() \
+            .where(JWNews.link == content['link']) \
+            .count() == 0
+
+        if not_already_sended:
+            self.links.append(content['link'])
+        return not_already_sended
+
+
+class JWVideosAPI(JWAPI):
+    api_url = ('https://b.jw-cdn.org/apis/mediator/v1/'
+        'categories/E/LatestVideos?detailed=1&clientType=www')
+    videos_url = 'https://www.jw.org/en/library/videos/#en/mediaitems/LatestVideos/'
+
+    def _map_videos(self, video):
+        video_link = self.videos_url + video['naturalKey']
+        image_link = video['images']['lss']['lg']
+        mapped_video = {
+            'link': f'[-]({image_link})*Video:*\n[{video["title"]}]({video_link})',
+            'date': video['firstPublished'][:10]
+        }
+        return mapped_video
+
+    def get_latest_videos(self):
+        response = requests.get(self.api_url)
+        if response.status_code != 200:
+            logger.error(f"Date {dt.now()}: {response.content}")
+            return []
+        media = response.json()['category']['media']
+        videos = map(self._map_videos, media)
+        videos = list(filter(self._filter_content, videos))
+        return videos
+
+
+class Parser(JWAPI):
     jw_site = 'https://www.jw.org'
 
     def _parse_html(self) -> BeautifulSoup:
@@ -25,18 +76,6 @@ class Parser:
             return BeautifulSoup(response.text, 'html.parser')
         with open("src/jw_news/news.html", "r") as f:
             return BeautifulSoup(f, "html.parser")
-
-    def _get_last_articles(self, num_articles: int) -> list:
-        try:
-            html = self._parse_html()
-        except Exception as e:
-            logging.error(f"Date {dt.now()}:\n{e}")
-            return []
-        new_articles_section = html.body.article.find(
-            "div", class_="whatsNewItems").find_all("div", class_="syn-body")
-
-        last_articles = new_articles_section[:num_articles]
-        return last_articles
 
     def _get_article_info(self, article) -> dict:
         article_info = {}
@@ -49,30 +88,25 @@ class Parser:
 
         return article_info
 
+    def _get_lastest_articles(self, num_articles: int=20) -> list:
+        try:
+            html = self._parse_html()
+        except Exception as e:
+            logging.error(f"Date {dt.now()}:\n{e}")
+            return []
+        new_articles_section = html.body.article.find(
+            "div", class_="whatsNewItems").find_all("div", class_="syn-body")
+
+        last_articles: list[BeautifulSoup] = new_articles_section[:num_articles]
+        articles = map(self._get_article_info, last_articles)
+        articles = filter(self._filter_content, articles)
+        return list(articles)
+
     def get_today_articles(self) -> list:
         """
         Returns a list of dictionaries representing the articles.
         """
-        today = date.today().strftime("%Y-%m-%d")  # Format: YYYY-MM-DD
-        yesterday = (date.today()-td(days=1)).strftime("%Y-%m-%d")  # Format: YYYY-MM-DD
-        last_articles = self._get_last_articles(20)
-        articles_json = []
-        articles_links = []
-        for article in last_articles:
-            new_article = self._get_article_info(article)
+        lastest_articles = self._get_lastest_articles()
+        latest_videos = JWVideosAPI().get_latest_videos()
 
-            repeated_link = new_article['link'] in articles_links
-            article_is_old = new_article['date'] not in [today, yesterday]
-
-            if repeated_link or article_is_old:
-                continue
-
-            not_already_sended = JWNews.select() \
-                .where(JWNews.link == new_article['link']) \
-                .count() == 0
-
-            if not_already_sended:
-                articles_json.append(new_article)
-                articles_links.append(new_article['link'])
-
-        return articles_json
+        return lastest_articles + latest_videos
--- src/settings/telegram.py
+++ src/settings/telegram.py
@@ -7,5 +7,6 @@ BRUNITO_BOT_URL = f"https://api.telegram.org/bot{BRUNITO_BOT_TOKEN}/"
 # Chats
 # -----------------------------------------------------------------------------
 AUDIO_VIDEO_GROUP_ID = -1001652887763
+JWNEWS_GROUP_ID = -737531569
 SANTIAGO_CHAT_ID = 1861335492
 FLOR_CHAT_ID = 1638521187
